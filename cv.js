f1af3c3d3be7fcf0e8323a5aded26b37279e9e0d
br1esc <77226337+br1esc@users.noreply.github.com>
2021-02-02 19:07:05 +0100

    Update cv.js

--- cv.js
+++ cv.js
@@ -7,20 +7,70 @@ function mostrar () {
     if (document.getElementById("selector").value == 'Mostrar todo'){
 
       document.getElementsByClassName("row-Formacion")[0].style.display='block';
+      document.getElementsByClassName("row-Formacion")[1].style.display='block';
+      document.getElementsByClassName("row-Formacion")[2].style.display='block';
+      document.getElementsByClassName("row-Formacion")[3].style.display='block';
+      document.getElementsByClassName("row-Formacion")[4].style.display='block';
+      document.getElementsByClassName("row-Formacion")[5].style.display='block';
+      document.getElementsByClassName("row-Formacion")[6].style.display='block';
+      document.getElementsByClassName("row-Formacion")[7].style.display='block';
+      document.getElementsByClassName("row-Formacion")[8].style.display='block';
+
+
 
       document.getElementsByClassName("row-Trabajo")[0].style.display='block';
+      document.getElementsByClassName("row-Trabajo")[1].style.display='block';
+      document.getElementsByClassName("row-Trabajo")[2].style.display='block';
+      document.getElementsByClassName("row-Trabajo")[3].style.display='block';
+      document.getElementsByClassName("row-Trabajo")[4].style.display='block';
+      document.getElementsByClassName("row-Trabajo")[5].style.display='block';
+      document.getElementsByClassName("row-Trabajo")[6].style.display='block';
+      document.getElementsByClassName("row-Trabajo")[7].style.display='block';
+      document.getElementsByClassName("row-Trabajo")[8].style.display='block';
+
+      
 
     } else if (document.getElementById("selector").value == 'Experiencia Laboral'){
 
         document.getElementsByClassName("row-Formacion")[0].style.display = 'none'; 
+        document.getElementsByClassName("row-Formacion")[1].style.display = 'none';
+        document.getElementsByClassName("row-Formacion")[2].style.display = 'none';
+        document.getElementsByClassName("row-Formacion")[3].style.display = 'none';
+        document.getElementsByClassName("row-Formacion")[4].style.display = 'none';
+        document.getElementsByClassName("row-Formacion")[5].style.display = 'none';
+        document.getElementsByClassName("row-Formacion")[6].style.display = 'none';
+        document.getElementsByClassName("row-Formacion")[8].style.display = 'none';
 
         document.getElementsByClassName("row-Trabajo")[0].style.display = 'block';
+        document.getElementsByClassName("row-Trabajo")[1].style.display = 'block';
+        document.getElementsByClassName("row-Trabajo")[2].style.display = 'block';
+        document.getElementsByClassName("row-Trabajo")[3].style.display = 'block';
+        document.getElementsByClassName("row-Trabajo")[4].style.display = 'block';
+        document.getElementsByClassName("row-Trabajo")[5].style.display = 'block';
+        document.getElementsByClassName("row-Trabajo")[6].style.display = 'block';
+        document.getElementsByClassName("row-Trabajo")[7].style.display = 'block';
+        document.getElementsByClassName("row-Trabajo")[8].style.display = 'block';
 
     } else {
 
-        document.getElementsByClassName("row-Formacion")[0].style.display = 'block';
+      document.getElementsByClassName("row-Formacion")[0].style.display = 'block'; 
+      document.getElementsByClassName("row-Formacion")[1].style.display = 'block';
+      document.getElementsByClassName("row-Formacion")[2].style.display = 'block';
+      document.getElementsByClassName("row-Formacion")[3].style.display = 'block';
+      document.getElementsByClassName("row-Formacion")[4].style.display = 'block';
+      document.getElementsByClassName("row-Formacion")[5].style.display = 'block';
+      document.getElementsByClassName("row-Formacion")[6].style.display = 'block';
+      document.getElementsByClassName("row-Formacion")[8].style.display = 'block';
 
         document.getElementsByClassName("row-Trabajo")[0].style.display = 'none';
+        document.getElementsByClassName("row-Trabajo")[1].style.display = 'none';
+        document.getElementsByClassName("row-Trabajo")[2].style.display = 'none';
+        document.getElementsByClassName("row-Trabajo")[3].style.display = 'none';
+        document.getElementsByClassName("row-Trabajo")[4].style.display = 'none';
+        document.getElementsByClassName("row-Trabajo")[5].style.display = 'none';
+        document.getElementsByClassName("row-Trabajo")[6].style.display = 'none';
+        document.getElementsByClassName("row-Trabajo")[7].style.display = 'none';
+        document.getElementsByClassName("row-Trabajo")[8].style.display = 'none';
 
     }
 
